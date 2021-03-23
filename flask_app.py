1ff544574bc598d3c8e9b2fef00bcae4278cd326
pxzitiv <80686013+pxzitiv@users.noreply.github.com>
2021-03-23 20:18:07 +0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def hello_name():
     name = request.args.get ('n', '')
-    return 'Hello, World!'
+    return 'Hello,' + name 
