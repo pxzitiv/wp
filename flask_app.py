ad472127fed001fdaed03a03cdb6156ad5015aa3
pxzitiv <80686013+pxzitiv@users.noreply.github.com>
2021-03-23 20:51:46 +0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def form():
-    return render_template('form.html')
+    name = request.args.get ('name', ' ')
+    if name == ' '
+        return render_template('form.html')
+    else 
+        return ('Hello' + name )
